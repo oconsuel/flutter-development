5c3d20cd3f661e66bf8350d9e9e4e3c5c02b9ca0
oconsuel <vlad.philippovich@list.ru>
2021-02-18 19:44:37 +0300

    add some changes lab 1

--- lib/labs/FirstLab.dart
+++ lib/labs/FirstLab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class FirstLabs extends StatelessWidget {
   @override
@@ -9,8 +10,15 @@ class FirstLabs extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Настроить приложение',
-              style: TextStyle(fontSize: 35.0),
+              'Настроить пользователя',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6
+                  .copyWith(fontSize: 30.0)
+                  .copyWith(color: Colors.white)
+                  .copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
             SizedBox(
               height: 100,
@@ -19,8 +27,9 @@ class FirstLabs extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 new RaisedButton(
-                  textColor: Colors.black,
-                  color: Colors.purple[200],
+                  padding: EdgeInsets.all(20),
+                  textColor: Colors.white,
+                  color: Colors.deepPurple[800],
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(80))),
                   child: Text('Посмотреть', style: TextStyle(fontSize: 20.0)),
@@ -28,7 +37,7 @@ class FirstLabs extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) {
-                        return DetailScreen();
+                        return new MyFor();
                       }),
                     );
                   },
@@ -42,38 +51,202 @@ class FirstLabs extends StatelessWidget {
   }
 }
 
-class DetailScreen extends StatelessWidget {
+enum GenderList { male, female }
+
+class MyFor extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyForm();
+}
+
+class MyForm extends State {
+  final _formKey = GlobalKey<FormState>();
+  GenderList _gender;
+  bool _agreement = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Подробная информация'),
-      ),
-      body: Center(
-        child: Column(
+          centerTitle: true,
+          title: Text(
+            "Изменение профиля",
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(fontSize: 25.0)
+                .copyWith(color: Colors.purple[500])
+                .copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          )),
+      body: Container(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.all(10.0),
           children: [
-            Card(
-              child: Column(
-                children: [
-                  Image.network(
-                    'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-202005?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1587460552755',
-                    width: 300,
+            Center(
+                child: new Form(
+              key: _formKey,
+              child: new Column(
+                children: <Widget>[
+                  new SizedBox(height: 20.0),
+                  new Text(
+                    'Имя пользователя:',
+                    style: TextStyle(fontSize: 20.0),
                   ),
-                  ListTile(
-                    // leading: Icon(Icons.album),
-                    title: Text('Macbook Pro 13, 2020'),
-                    subtitle: Text(
-                        'Intel Core i5,Turbo Boost up to 3.8GHz, 32GB, 1TB SSD'),
+                  new SizedBox(height: 10.0),
+                  new TextFormField(
+                    decoration: InputDecoration(labelText: 'Full name'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Пожалуйста введите свое имя';
+                      }
+                      return null;
+                    },
+                  ),
+                  new SizedBox(height: 20.0),
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            new Text(
+                              'Ваш пол:',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            RadioListTile(
+                              activeColor: Colors.purple,
+                              title: const Text('Мужской'),
+                              value: GenderList.male,
+                              groupValue: _gender,
+                              onChanged: (GenderList value) {
+                                setState(() {
+                                  _gender = value;
+                                });
+                              },
+                            ),
+                            RadioListTile(
+                              activeColor: Colors.purple,
+                              title: const Text('Женский'),
+                              value: GenderList.female,
+                              groupValue: _gender,
+                              onChanged: (GenderList value) {
+                                setState(() {
+                                  _gender = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            new Text(
+                              'Ваш возраст:',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              height: 110,
+                              child: SfRadialGauge(axes: <RadialAxis>[
+                                RadialAxis(
+                                    axisLineStyle: AxisLineStyle(
+                                        thickness: 0.2,
+                                        thicknessUnit: GaugeSizeUnit.factor),
+                                    showTicks: false,
+                                    showLabels: false,
+                                    radiusFactor: 1,
+                                    pointers: <GaugePointer>[
+                                      RangePointer(
+                                          value: 20,
+                                          width: 0.2,
+                                          sizeUnit: GaugeSizeUnit.factor,
+                                          color: Color(0XFF6A1B9A)),
+                                      MarkerPointer(
+                                        value: 18,
+                                        color: Colors.white,
+                                        markerHeight: 5,
+                                        markerWidth: 5,
+                                        markerType: MarkerType.circle,
+                                      ),
+                                    ],
+                                    annotations: <GaugeAnnotation>[
+                                      GaugeAnnotation(
+                                          widget: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: const <Widget>[
+                                              Text(
+                                                '20',
+                                                style: TextStyle(
+                                                    fontSize: 15,
+                                                    fontFamily: 'Times',
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Color(0XFFCE93D8)),
+                                              ),
+                                              Text(
+                                                ' years',
+                                                style: TextStyle(
+                                                    fontSize: 15,
+                                                    fontFamily: 'Times',
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Color(0XFFCE93D8)),
+                                              )
+                                            ],
+                                          ),
+                                          positionFactor: 0.13,
+                                          angle: 0)
+                                    ])
+                              ]),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  new SizedBox(height: 20.0),
+                  new CheckboxListTile(
+                      activeColor: Colors.purple,
+                      value: _agreement,
+                      title: new Text('Я ознакомлен(a) с изменениями'),
+                      onChanged: (bool value) =>
+                          setState(() => _agreement = value)),
+                  new SizedBox(height: 20.0),
+                  new RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Color color = Colors.deepPurple[300];
+                        String text;
+
+                        if (_gender == null)
+                          text = 'Выберите свой пол';
+                        else if (_agreement == false)
+                          text = 'Необходимо подтвердить согласие';
+                        else {
+                          text = 'Форма успешно заполнена';
+                          color = Colors.deepPurple;
+                          Navigator.pop(context);
+                        }
+
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                          content: Text(text),
+                          backgroundColor: color,
+                        ));
+                      }
+                    },
+                    padding: EdgeInsets.all(20),
+                    child:
+                        Text('Подтвердить', style: TextStyle(fontSize: 20.0)),
+                    color: Colors.deepPurple[800],
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(80))),
                   ),
                 ],
               ),
-            ),
-            RaisedButton(
-              child: Text('Назад'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+            )),
           ],
         ),
       ),
--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:lab_1/utils/constants.dart';
 
 ThemeData basicTheme() => ThemeData(
-    brightness: Brightness.dark,
-    primaryColor: myFirstColor,
-    textTheme: TextTheme(
-      headline6: TextStyle(
-        fontFamily: FontNameTitle,
-        fontSize: MediumTextSize,
-        color: Colors.purple,
+      brightness: Brightness.dark,
+      primaryColor: myFirstColor,
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          fontFamily: FontNameTitle,
+          fontSize: MediumTextSize,
+          color: Colors.purple,
+        ),
+        bodyText1: TextStyle(
+          fontFamily: FontNameDefault,
+          fontSize: BodyTextSize,
+          color: Colors.green,
+        ),
       ),
-      bodyText1: TextStyle(
-        fontFamily: FontNameDefault,
-        fontSize: BodyTextSize,
-        color: Colors.green,
+      inputDecorationTheme: InputDecorationTheme(
+        border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple)),
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple)),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple)),
+        errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple)),
+        focusedErrorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple)),
       ),
-    ));
+    );
